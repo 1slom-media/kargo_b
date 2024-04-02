72e1f411250587d3257f91c0878d3455c81edbdf
1slom-media <islombektagayev1@gmail.com>
2024-04-02 13:12:25 +0500

    env config

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,13 +4,15 @@ import { UsersEntity } from "./entities/users";
 import { KargosEntity } from "./entities/kargos";
 import { ClientsEntity } from "./entities/clients";
 import { OrdersEntity } from "./entities/orders";
+import dotenv from 'dotenv';
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
   username: "postgres",
-  password: "islom_01",
+  password: process.env.PG_PASSWORD,
   database: "kargo_b",
   synchronize: true,
   logging: false,
